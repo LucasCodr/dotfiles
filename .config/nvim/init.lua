c82d55a574eee71c47ec01476abe00b666084d74
Lucas Souza <me@lucassouza.dev>
2025-01-03 07:28:58 -0300

    add telescope and organize plugins in separate files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,13 @@
-require("config/lazy")
-require("config/theme")
-require("config/lualine")
-require("config/treesitter")
+require("config.lazy")
+require("config.theme")
+require("config.lualine")
+require("config.treesitter")
+require("config.cmp")
+require("config.lsp")
+require("config.telescope")
+
+local set = vim.opt
+
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
